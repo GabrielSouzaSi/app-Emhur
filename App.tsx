3ffdad08c9d4cd9f5babf304cdb081c1b7706964
GabrielSouzaSi <gos.gabri@gmail.com>
2024-07-24 14:16:12 -0400

    Configuração do splash e fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [isFontLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text className="">Open up App.tsx to start working on your app!</Text>
