e12e1f85480ff67c9a24b24ee14da7f4a7397069
GabrielSouzaSi <gos.gabri@gmail.com>
2025-04-02 21:43:45 -0400

    database

--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -1,7 +1,7 @@
 // This file is required for Expo/React Native SQLite migrations - https://orm.drizzle.team/quick-sqlite/expo
 
 import journal from './meta/_journal.json';
-import m0000 from './0000_nosy_cassandra_nova.sql';
+import m0000 from './0000_jazzy_grandmaster.sql';
 
   export default {
     journal,
