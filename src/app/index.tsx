e78e2bf1a320d4f8ce29be7d5e1392880d6c98a0
GabrielSouzaSi <gos.gabri@gmail.com>
2025-04-25 15:17:10 -0400

    Login visualmente mais simples

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Image, Text, View, Pressable, Alert, Keyboard } from "react-native";
+import {
+  Image,
+  Text,
+  View,
+  Pressable,
+  Alert,
+  Keyboard,
+  ScrollView,
+} from "react-native";
 import { useRouter } from "expo-router";
 import { useForm, Controller } from "react-hook-form";
 
@@ -33,82 +41,87 @@ export default function Login() {
       await signIn(email.toLowerCase(), password);
     } catch (error) {
       console.log("Error =>", error);
-      Alert.alert("Aviso!","Não foi possível fazer login, tente novamente!");
+      Alert.alert("Aviso!", "Não foi possível fazer login, tente novamente!");
     } finally {
-      Keyboard.dismiss()
+      Keyboard.dismiss();
       setIsLoading(false);
     }
   }
 
   return (
     <>
-      <Header title="Login" />
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <View className="flex-1 p-8 justify-center">
+          <View className="items-center mb-10">
+            <Image
+              className="w-48 h-48"
+              source={require("@/assets/adaptive-icon.png")}
+            />
+          </View>
 
-      <View className="flex-1 p-8 justify-center">
-        <View className="items-center mb-10">
-        <Image className="w-48 h-48" source={require("@/assets/adaptive-icon.png")} />
-        </View>
+          <View>
+            <Controller
+              control={control}
+              name="email"
+              rules={{
+                required: "Informe o e-mail, matrícula ou código!",
+                // pattern: {
+                //   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                //   message: "E-mail inválido",
+                // },
+              }}
+              render={({ field: { onChange } }) => (
+                <Field
+                  className={`${!!errors.email ? "border-red-500" : ""}`}
+                  placeholder="E-mail, Matrícula, Código"
+                  onChangeText={onChange}
+                />
+              )}
+            />
 
-        <View>
-          <Controller
-            control={control}
-            name="email"
-            rules={{
-              required: "Informe o e-mail, matrícula ou código!",
-              // pattern: {
-              //   value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-              //   message: "E-mail inválido",
-              // },
-            }}
-            render={({ field: { onChange } }) => (
-              <Field
-                className={`${!!errors.email ? "border-red-500" : ""}`}
-                placeholder="E-mail, Matrícula, Código"
-                onChangeText={onChange}
-              />
+            {errors.email?.message && (
+              <Text className="font-regular font-bold text-lg text-red-500">
+                {errors.email.message}
+              </Text>
             )}
-          />
 
-          {errors.email?.message && (
-            <Text className="font-regular font-bold text-lg text-red-500">
-              {errors.email.message}
-            </Text>
-          )}
-
-          <Controller
-            control={control}
-            name="password"
-            rules={{ required: "Informe a senha!" }}
-            render={({ field: { onChange } }) => (
-              <Field
-                className={`mt-5 ${!!errors.password ? "border-red-500" : ""}`}
-                placeholder="Senha"
-                secureTextEntry
-                textContentType="password"
-                onChangeText={onChange}
-                onSubmitEditing={() => handleSubmit(handleSignIn)}
-                returnKeyType="send"
-              />
+            <Controller
+              control={control}
+              name="password"
+              rules={{ required: "Informe a senha!" }}
+              render={({ field: { onChange } }) => (
+                <Field
+                  className={`mt-5 ${
+                    !!errors.password ? "border-red-500" : ""
+                  }`}
+                  placeholder="Senha"
+                  secureTextEntry
+                  textContentType="password"
+                  onChangeText={onChange}
+                  onSubmitEditing={() => handleSubmit(handleSignIn)}
+                  returnKeyType="send"
+                />
+              )}
+            />
+            {errors.password?.message && (
+              <Text className="font-regular font-bold text-lg text-red-500">
+                {errors.password.message}
+              </Text>
             )}
-          />
-          {errors.password?.message && (
-            <Text className="font-regular font-bold text-lg text-red-500">
-              {errors.password.message}
+          </View>
+
+          <Pressable className="my-7 items-end">
+            <Text className="font-regular font-bold text-base text-blue-500">
+              Esqueceu a senha?
             </Text>
-          )}
+          </Pressable>
+
+          <Button variant="primary" onPress={handleSubmit(handleSignIn)}>
+            <Button.TextButton title="Entrar" />
+          </Button>
         </View>
-
-        <Pressable className="my-7 items-end">
-          <Text className="font-regular font-bold text-base text-blue-500">
-            Esqueceu a senha?
-          </Text>
-        </Pressable>
-
-        <Button variant="primary" onPress={handleSubmit(handleSignIn)}>
-          <Button.TextButton title="Entrar" />
-        </Button>
-      </View>
-        {isLoading && <Loading />}
+      </ScrollView>
+      {isLoading && <Loading />}
     </>
   );
 }
