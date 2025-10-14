e40d7d117a3973d45180d443afe52046d68a9ecc
GabrielSouzaSi <gos.gabri@gmail.com>
2025-10-14 15:46:14 -0400

    feat(violations): adicionar novos campos à tabela de violações e atualizar os arquivos de migração

---
+++ drizzle/0002_smooth_goblin_queen.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `violations` ADD `assistenteName` text;--> statement-breakpoint
+ALTER TABLE `violations` ADD `assistenteCPF` text;--> statement-breakpoint
+ALTER TABLE `violations` ADD `assistenteCNH` text;--> statement-breakpoint
+ALTER TABLE `violations` ADD `signatureUri` text;
--- drizzle/migrations.js
+++ drizzle/migrations.js
@@ -3,12 +3,14 @@
 import journal from './meta/_journal.json';
 import m0000 from './0000_jazzy_grandmaster.sql';
 import m0001 from './0001_cooing_speed.sql';
+import m0002 from './0002_smooth_goblin_queen.sql';
 
   export default {
     journal,
     migrations: {
       m0000,
-m0001
+m0001,
+m0002
     }
   }
   
