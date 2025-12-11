29fffa2a869a23064e332d33165a21ac3201e9d2
GabrielSouzaSi <gos.gabri@gmail.com>
2025-12-11 00:31:32 -0400

    feat(input): add error message display to Input component

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,23 +1,41 @@
-import { TextInput, TextInputProps } from "react-native";
+import { Text, TextInput, TextInputProps } from "react-native";
 import clsx from "clsx";
 
 type Variants = "primary" | "secundary";
 
 type InputProps = TextInputProps & {
   variant?: Variants;
+  errorMessage?: string;
 };
 
-function Field({ className, variant = "secundary", ...rest }: InputProps) {
+function Field({
+  className,
+  variant = "secundary",
+  errorMessage,
+  ...rest
+}: InputProps) {
   return (
-    <TextInput
-      className={clsx(
-        "h-16 border-gray-400 border-2 bg-white font-semiBold text-lg rounded-md px-4",
-        { "focus:border-blue-500": variant === "primary" },
-        { "focus:border-green-500": variant === "secundary" },
-        className
+    <>
+      <TextInput
+        autoCorrect={false}
+        spellCheck={false}
+        autoCapitalize="none"
+        textBreakStrategy="simple"
+        importantForAutofill="no"
+        keyboardType="default"
+        underlineColorAndroid="transparent"
+        className={clsx(
+          "h-16 border-2 bg-white font-semiBold text-lg rounded-md px-4 focus:border-blue-500",
+          { "border-gray-400": !errorMessage },
+          { "border-red-400": errorMessage },
+          className
+        )}
+        {...rest}
+      />
+      {errorMessage && (
+        <Text className="text-red-500 mt-1 ml-1">{errorMessage}</Text>
       )}
-      {...rest}
-    />
+    </>
   );
 }
 
