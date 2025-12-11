31e2359b8ba28c15114a81c04469bf7d56201854
GabrielSouzaSi <gos.gabri@gmail.com>
2025-12-11 00:32:09 -0400

    feat(modal): add RNModal component for customizable modal display with optional input

---
+++ src/components/RNModal.tsx
@@ -0,0 +1,42 @@
+import {
+  Modal as RNModal,
+  ModalProps,
+  KeyboardAvoidingView,
+  View,
+  Platform,
+} from "react-native";
+type Props = ModalProps & {
+  isOpen: boolean;
+  withInput?: boolean;
+};
+
+export const Modal = ({
+  isOpen,
+  withInput = false,
+  children,
+  ...rest
+}: Props) => {
+  const content = withInput ? (
+    <KeyboardAvoidingView
+      className="items-center justify-center flex-1 px-3 bg-zinc-900/40"
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      {children}
+    </KeyboardAvoidingView>
+  ) : (
+    <View className="items-center justify-center flex-1 px-3 bg-zinc-900/40">
+      {children}
+    </View>
+  );
+  return (
+    <RNModal
+      visible={isOpen}
+      transparent
+      animationType="fade"
+      statusBarTranslucent
+      {...rest}
+    >
+      {content}
+    </RNModal>
+  );
+};
